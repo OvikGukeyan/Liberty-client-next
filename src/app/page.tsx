8d9860117b003a3e34d4811c519937457620aabb
Ovik Gukeyan <0953188061ovik@gmail.com>
2024-05-03 12:54:01 +0200

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,108 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import Carousel from "@/components/Carousel";
+import Slide from "@/components/Slide"
+import Footer from "@/components/Footer";
+
+const servicesList = [
+  {
+    name: "Versicherung",
+    text: "Wir bieten maßgeschneiderte Versicherungslösungen für Ihr Zuhause an. Schützen Sie Ihr Eigentum vor unvorhergesehenen Ereignissen mit unseren flexiblen Versicherungsprodukten. Verlassen Sie sich auf uns, um Ihre Bedürfnisse zu verstehen und Ihnen den richtigen Schutz zu bieten.",
+  },
+  {
+    name: "Baufinanzierung",
+    text: "Sichern Sie sich Ihr Traumhaus mit unserer maßgeschneiderten Baufinanzierungslösung. Egal, ob Neubau, Kauf oder Umbau - wir bieten Ihnen flexible Finanzierungsoptionen, um Ihr Bauprojekt zu realisieren. Vertrauen Sie auf unsere langjährige Erfahrung, um Ihre individuellen Bedürfnisse zu verstehen und Ihnen die passende Baufinanzierung anzubieten.",
+  },
+  {
+    name: "Privatkredit",
+    text: "Erfüllen Sie sich Ihre persönlichen Wünsche mit unserem maßgeschneiderten Privatkredit. Egal, ob Sie eine Reise planen, ein Auto kaufen oder eine Renovierung vornehmen möchten - wir bieten Ihnen flexible Kreditoptionen, um Ihre finanziellen Ziele zu erreichen. Verlassen Sie sich auf unsere Expertise, um Ihnen den richtigen Privatkredit anzubieten.",
+  },
+  {
+    name: "Autokredit",
+    text: "Machen Sie sich den Autokauf leicht mit unserem maßgeschneiderten Autokredit. Egal, ob Neu- oder Gebrauchtwagen - wir bieten Ihnen flexible Finanzierungsmöglichkeiten, um Ihren Autokauf zu erleichtern. Vertrauen Sie auf unsere Kompetenz, um Ihnen den passenden Autokredit anzubieten.",
+  },
+  {
+    name: "Kapitalaufbau",
+    text: "Planen Sie Ihre finanzielle Zukunft mit unserem maßgeschneiderten Kapitalaufbau-Programm. Egal, ob Sie für den Ruhestand vorsorgen, ein finanzielles Polster schaffen oder langfristig investieren möchten - wir bieten Ihnen strategische Anlageoptionen, um Ihr Kapital aufzubauen. Verlassen Sie sich auf unsere Experten, um Ihnen dabei zu helfen, Ihre finanziellen Ziele zu erreichen.",
+  },
+  {
+    name: "Immobilien",
+    text: "Investieren Sie in Ihre Zukunft mit unserer maßgeschneiderten Immobilienlösung. Egal, ob Sie kaufen, verkaufen oder investieren möchten - wir bieten Ihnen umfassende Beratung und Unterstützung, um Ihre Immobilienziele zu erreichen. Vertrauen Sie auf unsere Erfahrung, um Ihnen bei jedem Schritt des Immobilienprozesses zu helfen.",
+  },
+];
+
+export const metadata = {
+  title: "About Us",
+  description: "Info about us",
+  keywords: ''
+};
 
 export default function Home() {
+  const isMenuVisiable = false
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <div className={styles.wrapper}>
+      <div className={styles.box_1}>
+        <div className={styles.main}>
+          <video
+            muted
+            autoPlay
+            className={styles.video}
+            src="./assets/Lingen1.mp4"
+          ></video>
+          <header>
+            <img
+              // onClick={handleHamburgerClick}
+              className={styles.hamburger}
+              src="./assets/hamburger.png"
+              alt=""
             />
-          </a>
+            <div className={styles.logo}>
+              <img src="./assets/logo-full.png" alt="" />
+            </div>
+            <div
+              // onClick={(e) => handleOutsideClick(e)}
+              className={`${styles.overlay} ${
+                isMenuVisiable ? styles.overlayVisible : ""
+              }`
+            }
+            >
+              <nav >
+                <img 
+                // onClick={handleHamburgerClick} 
+                className={styles.close} src="./assets/close.png" alt="" />
+                <ul>
+                  <li>home</li>
+                  <li>shope</li>
+                  <li>about</li>
+                  <li>contact</li>
+                </ul>
+              </nav>
+            </div>
+          </header>
+        </div>
+        <div className={styles.services}>
+          <Carousel>
+          {servicesList.map((item) => (
+            <Slide item={item} />
+          ))}
+        </Carousel>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+      <div className={styles.box_2}>
+        <div className={styles.photo_1}></div>
+        <div className={styles.about}>
+          <h3>ÜBER UNSERE FIRMA</h3>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Willkommen bei Liberty Finance, Ihrem zuverlässigen Partner für
+            Versicherungs- und Finanzierungslösungen. Wir bieten
+            maßgeschneiderte Produkte für Hausbesitzer an, um ihr Eigentum zu
+            schützen und ihre Wohnträume zu verwirklichen. Kontaktieren Sie uns
+            für professionelle Beratung und persönlichen Service.
           </p>
-        </a>
+        </div>
+        <div className={styles.photo_2}></div>
       </div>
-    </main>
+      <Footer/>
+    </div>
   );
 }
---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import React, {
+  useEffect,
+  useState,
+  Children,
+  ReactNode,
+  ReactElement,
+  useRef,
+} from "react";
+import styles from "./Carousel.module.scss";
+
+type CarouselTypes = {
+  children: ReactNode;
+};
+
+const Carousel: React.FC<CarouselTypes> = ({ children }) => {
+  const [pages, setPages] = useState<ReactNode[]>([]);
+  const [offset, setOffset] = useState(0);
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const [sliderWidth, setSliderWidth] = useState(0);
+
+  const handleLeftArrowClick = () => {
+    setOffset((currentOffset) => {
+      const newOffset = currentOffset + sliderWidth;
+      const maxOffset = -(sliderWidth * (pages.length - 1));
+      const result = newOffset <= 0 ? newOffset : maxOffset;
+      // return Math.min(newOffset, 0)
+      return result;
+    });
+  };
+  const handleRightArrowClick = () => {
+    setOffset((currentOffset) => {
+      const newOffset = currentOffset - sliderWidth;
+      const maxOffset = -(sliderWidth * (pages.length - 1));
+      const result = newOffset >= maxOffset ? newOffset : 0;
+
+      // Math.max(newOffset, maxOffset)
+      return result;
+    });
+  };
+
+  const handleResize = () => {
+    if (sliderRef.current) {
+      const width = sliderRef.current.offsetWidth;
+      setSliderWidth(width);
+      setOffset(0);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const newChildren = Children.map(children, (child) => {
+      if (React.isValidElement(child)) {
+        return (
+          <div
+            style={{
+              minWidth: "100%",
+              maxWidth: "100%",
+              height: "100%",
+              ...child.props.style,
+            }}
+          >
+            {child}
+          </div>
+        );
+      }
+      return null;
+    }) as ReactElement[];
+    setPages(newChildren);
+
+    
+  }, [children]);
+
+  
+
+  
+
+  return (
+    <div className={styles.main_container}>
+      <div ref={sliderRef} className={styles.window}>
+        <div
+          className={styles.all_pages_container}
+          style={{
+            transform: `translateX(${offset}px)`,
+          }}
+        >
+          {pages}
+        </div>
+
+        <div className={styles.slider}>
+          <div onClick={handleLeftArrowClick} className={styles.button_left}>
+            <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M13 0L1 12l12 12" fill="none" fillRule="evenodd" />
+            </svg>
+          </div>
+          <div onClick={handleRightArrowClick} className={styles.button_right}>
+            <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 0l12 12L1 24" fill="none" fillRule="evenodd" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
