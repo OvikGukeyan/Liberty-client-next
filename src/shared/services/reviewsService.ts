65f3380eb6ee98e10217182338579bad36f8e7ca
Ovik Gukeyan <0953188061ovik@gmail.com>
2025-02-04 19:47:34 +0100

    api-url

--- src/shared/services/reviewsService.ts
+++ src/shared/services/reviewsService.ts
@@ -4,7 +4,7 @@ import { ReviewType } from "../models/Review";
 export default class reviewsService {
   static async getReviews(): Promise<ReviewType[] | undefined> {
     try {
-      const { data } = await axios.get("http://localhost:7777/api/reviews");
+      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/reviews`);
       return data;
     } catch (error) {
       console.error(error);
